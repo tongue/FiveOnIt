e27b8e9c1ce4abf1af6fe506fef5dbfa06fc0abb
jesrho <jesper.rhodin@gmail.com>
2013-08-29 18:31:30 +0200

    app.js

--- app.js
+++ app.js
@@ -33,6 +33,101 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+var server = http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+var io = require('socket.io').listen(server);
+
+
+var clients = [];
+
+io.sockets.on('connection', function(socket){
+
+	socket.on('joinGame', function(data)
+	{
+		clients.push(socket);
+		clients[clients.indexOf(socket)].username = data.username;
+
+		
+		socket.emit('preloadGame', 
+		{
+			imageUrl: 'http://thinkprogress.org/wp-content/uploads/2013/01/Google-300x168.jpg'
+		})
+
+		
+		var noClients = clients.length;
+		console.log(clients);
+		
+		if(noClients >= 2)
+		{
+			io.sockets.emit('showReady', true);
+		}		
+
+	})
+
+	socket.on('clientReady', function(data){
+		
+		clients[clients.indexOf(socket)].isReady = data.ready;
+
+		console.log('client is: ', 
+			clients[clients.indexOf(socket)].username, 'and: ', 
+			clients[clients.indexOf(socket)].isReady);
+
+		var allIsReady = true;
+		clients.forEach(function(index, client){
+			if(allIsReady)
+				allIsReady = clients[clients.indexOf(socket)].isReady;
+		});
+
+		if(allIsReady)
+			io.sockets.emit('startGame', { noClients: clients.length } )
+
+	})
+
+	socket.on('disconnect', function(){
+		clients.splice(clients.indexOf(socket), 1);
+	});
+
+
+	socket.on('clientClick', function(data)
+	{
+		var hit = detectHit(data.coordinates, clients[clients.indexOf(socket)].hitArea);
+		var win = false;
+		
+		if(hit)
+		{
+			clients[clients.indexOf(socket)].points += 1;
+			win = (clients[clients.indexOf(socket)].points > 4);
+		}
+
+		if(!win)
+		{
+			if(hit)
+			{
+				clients[clients.indexOf(socket)].points += 1;
+			}
+			var callbackObject = {x = 0, y = 0};
+
+			socket.emit('clickCallback', callbackObject);
+		}
+		else
+			io.sockets.emit('gameOver', clients);
+	})
+});
+
+function detectHit(coord, rect) {
+  return !(
+        ((a.y + a.h) < (b.y)) ||
+        (a.y > (b.y + b.h)) ||
+        ((a.x + a.w) < b.x) ||
+        (a.x > (b.x + b.w))
+    );
+}
+function Rect(x,y,w,h){
+this.x = x;
+this.y = y;
+this.w = w;
+this.h = h;
+
+}
