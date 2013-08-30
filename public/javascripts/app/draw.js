505a263c6c921590e9f014677f8b1156a9413a41
andlinds <andreaslindstrom87@gmail.com>
2013-08-30 09:48:21 +0200

    draw.js

--- public/javascripts/app/draw.js
+++ public/javascripts/app/draw.js
@@ -2,6 +2,14 @@ define( ['jquery'], function ( $ ) {
 	return {
 		drawHit: function( context, x, y ) {
 			var dimension = 55;
+			context.beginPath();
+			context.globalAlpha = 0.5
+			context.rect(x, y, dimension, dimension);
+			context.fillStyle = 'green';
+			context.fill();
+			context.lineWidth = 1;
+			context.strokeStyle = 'black';
+			context.stroke();
 			
 		}
 	}
